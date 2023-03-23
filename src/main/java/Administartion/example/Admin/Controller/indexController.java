cd4a0841b346e97c8cc28aa9cf6d2b8a42509b99
YunHyeok <djbyh645047@naver.com>
2023-03-23 19:58:55 +0900

    html layout

--- src/main/java/Administartion/example/Admin/Controller/indexController.java
+++ src/main/java/Administartion/example/Admin/Controller/indexController.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class indexController {
 
-    @GetMapping("Index")
-
-    public static String index() {
-        return "index";
+    @GetMapping("/")
+    public static String main() {
+        return "dashboard";
     }
+
+    @GetMapping("login")
+
+    public static String login() {
+        return "login";
+    }
+
 }
